f7c6508b29b5b8a893f658310bc3efc0a7ef2839
Loren Barrick <lorenbarrick@gmail.com>
2016-06-10 12:41:37 -0700

    gymSearch successfully  displays data, working on other api endpoints

--- client/actions/gyms-actions.js
+++ client/actions/gyms-actions.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const FETCH_GYMS = 'FETCH_GYMS';
 
 export const fetchGyms = (address, distance) => {
-  const axiosPromise = axios.post('/gym', {address, distance});
+  const axiosPromise = axios.post('/gym', {address, distance})
+    .then((response) => response.data);
   return {
     type: FETCH_GYMS,
     payload: axiosPromise
--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise-middleware';
 
 import reducers from './reducers';
-import GymSearch from './components/GymSearch';
+import GymSearch from './containers/GymSearch';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise())(createStore);
 
