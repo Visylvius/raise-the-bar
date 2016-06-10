5f4254ba20b674fe59b584ac963f2f2ab707bba9
Loren Barrick <lorenbarrick@gmail.com>
2016-06-09 23:36:46 -0700

    fix typo

--- client/reducers/gyms-reducer.js
+++ client/reducers/gyms-reducer.js
@@ -6,7 +6,7 @@ export const gymsReducer = (state=initialState, action) => {
   switch (action.type) {
     case `${FETCH_GYMS}_PENDING`:
     return initialState;
-    case `${FETCH_GYMS}_FUFILLED`:
+    case `${FETCH_GYMS}_FULFILLED`:
     return { listOfGyms: action.payload, error: null, loaded: true};
     case `${FETCH_GYMS}_REJECTED`:
     return { listOfGyms: null, error: action.payload, loaded: false};
