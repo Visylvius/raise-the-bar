5f22045b3f4a0ca5a7aa7c6d53a4d11440131a49
Loren Barrick <lorenbarrick@gmail.com>
2016-06-23 13:56:58 -0700

    changed class to classNamee, bootstrap working properly

--- client/components/menu-bar.js
+++ client/components/menu-bar.js
@@ -6,19 +6,19 @@ const styles = {};
 const MenuBar = (props) => {
   return (
     <div>
-      <nav class="navbar navbar-dark bg-inverse">
-        <ul class="nav navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <nav className="navbar navbar-dark bg-inverse">
+        <ul className="nav navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Athletes</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Athletes</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Trainers</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Trainers</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Find Gyms Near You</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Find Gyms Near You</a>
           </li>
         </ul>
       </nav>
