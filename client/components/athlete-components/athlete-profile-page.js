1d67dbcafcbb0108d7e4ce79654452071dbcc3c0
Loren Barrick <lorenbarrick@gmail.com>
2016-06-28 11:55:02 -0700

    include radium stylesheet, working on style tags in jsx

--- client/components/athlete-components/athlete-profile-page.js
+++ client/components/athlete-components/athlete-profile-page.js
@@ -1,6 +1,87 @@
 import React from 'react';
 import Radium from 'radium';
 
+const baseStyles = {
+  upperContainer: {
+    marginTop: '20px',
+    backgroundColor: '#blue';
+  },
+  pageContainer: {
+    height: '100vh',
+    backgroundColor: '#E6E6E6'
+  },
+  profileTop: {
+    height: '123px',
+    backgroundColor: '#E6E5E4',
+    borderRadius: '5px 5px 0px 0px'
+  },
+  profileBottom: {
+    height: '400px',
+    backgroundColor: 'FFF';
+  },
+  something: {
+    marginTop: '15px',
+    marginLeft: '18px',
+    height: '150px',
+    width: '200px',
+    borderRadius: '5px'
+  },
+  athleteName: {
+    marginLeft: '270px',
+    marginTop: -'140px',
+    fontSize: '22px';
+    fontWeight: 'bold'
+  },
+  liftingStyle: {
+    marginLeft: '270px',
+    fontSize: '18px',
+    color: '#C9C9C8'
+  },
+  navigationLinks: {
+    marginTop: -'24px',
+    marginLeft: '40px'
+  },
+  lineBreak: {
+    marginTop: '60px'
+  },
+  icons: {
+    marginLeft: '20px'
+  },
+  hrSpace: {
+    width: '195px',
+    marginRight: '90px'
+  },
+  mainBio: {
+    paddingTop: '10px',
+    marginRight: '400px',
+    fontSize: '20px',
+    width: '200px'
+  },
+  profileNavigation: {
+    width: '335px'
+  },
+  bioText: {
+    height: '250px',
+    width: '500px',
+    border: '1px solid blue',
+    marginLeft: '150px',
+    marginTop: -'20px'
+  },
+  mainBioData: {
+    border: '1px solid black'
+  },
+  bioDataMain: {
+    width: '500px',
+    wordWrap: 'break-word'
+  },
+  bioAbout: {
+    marginLeft: '48px'
+  },
+  mainBioHr: {
+    marginRight: '330px',
+    marginBottom: -'40px'
+  }
+}
 
 const AthleteProfile = (props) => {
   return (
