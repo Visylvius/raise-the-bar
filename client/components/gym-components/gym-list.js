117d2a568033829e7e7145ccd75c9e6d0217cbe2
Loren Barrick <lorenbarrick@gmail.com>
2016-10-26 09:41:17 -0700

    photos now rendering for gym search

--- client/components/gym-components/gym-list.js
+++ client/components/gym-components/gym-list.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import GymCard from './gym-card.js';
 
@@ -6,25 +8,42 @@ const GymList = ( { listOfGyms } ) => {
   if (listOfGyms === null) {
     return null;
   }
-  console.log('list of gyms', listOfGyms);
-  const renderGyms = gymData => {
-    console.log('gym data', gymData);
-    return (
-      <GymCard
-        id={gymData.id}
-        key={gymData.id}
-        placeId={gymData.place_id}
-        name={gymData.name}
-        address={gymData.address}
-      />
-    );
-  };
+
+  const getGymPhotoUrl = (photoReference) =>
+     'https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=' +
+      encodeURIComponent(photoReference) +
+      '&sensor=true&key=AIzaSyALGeTDHSBu-A1D8FltPiVBlgJZU7Cpmp0'
+
+
+  console.log(listOfGyms);
+
   return (
     <div>
-      {listOfGyms.results.map(renderGyms)}
+      {listOfGyms.results.map((gym) => {
+        return (
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title={gym.name} subtitle={gym.vicinity} />}
+            >
+            { gym.photos && gym.photos[0]
+              ? <img src={getGymPhotoUrl(gym.photos[0].photo_reference)} />
+              : <img src='http://placekitten.com/300/200' />
+            }
+
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+            </CardActions>
+          </Card>
+        )
+      })}
     </div>
-
-
   );
 };
 
@@ -35,4 +54,29 @@ const GymList = ( { listOfGyms } ) => {
 //   <div>{gymData.address}</div>
 // </div> */}
 
+
 export default GymList;
+
+
+// {listOfGyms.results.map((gym) => {
+//   return (
+//     <Card>
+//       <CardMedia
+//         overlay={<CardTitle title={gym.name} subtitle={gym.vicinity} />}
+//       >
+//         <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=${gym.photos[0].photo_reference}&sensor=true&key=${process.env.API_KEY}`} />
+//       </CardMedia>
+//
+//       <CardText>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+//         Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+//         Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+//         Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+//       </CardText>
+//       <CardActions>
+//         <RaisedButton label="Learn more about this gym" />
+//       </CardActions>
+//     </Card>
+//   )
+// })
+// }
--- client/components/inbox-components/inbox.js
+++ client/components/inbox-components/inbox.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import Radium from 'radium';
 
-const Inbox = () => {
+
+import { connect } from 'react-redux';
+
+const Inbox = ({messages}) => {
+  if (messages === null) {
+    return null;
+  }
   return (
-    <div>This is the inbox for now</div>
+    <div className='user-container' style={styles.container}>
+      <ul className='user-list' style={styles.userList}>
+        {messages.map((message) => {
+          return (
+            <li className='user' style={styles.user}>
+              <div className='avatar' style={styles.avatarContainer}><img style={styles.avatar} className='user-photo' src={`avatars/athlete/test.jpg`}/></div>
+              <div className='message-container' style={styles.messageContainer}>
+                <h4 className='user-name' style={styles.userName}>{message.from}</h4>
+                <div className='user-message'>{message.body}</div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   );
 };
 
-export default Inbox;
+const styles = {
+  container: {
+    border: '1px solid black',
+    height: '500px',
+    width: '100%',
+    maxWidth: '640px',
+    backgroundColor: 'red',
+    margin: '0 auto'
+  },
+  userList: {
+    padding: 0,
+    listStyle: 'none'
+  },
+  user: {
+    height: '160px'
+  },
+  avatarContainer: {
+    height: '100%',
+    display: 'inline-block',
+    width: '30%',
+    verticalAlign: 'top'
+  },
+  messageContainer: {
+    height: '100%',
+    display: 'inline-block',
+    width: '70%',
+    verticalAlign: 'top',
+    paddingLeft: '10px'
+  },
+  avatar: {
+    height: 'auto',
+    width: '100%'
+  },
+  userName: {
+    marginBottom: '5px'
+  }
+};
+
+//TODO make sure if number of characters is > 66 then show ...
+
+const mapStateToProps = (state) => {
+  const messages = state.inbox.messages;
+  return { messages };
+};
+
+export default connect(mapStateToProps, null)(Inbox);
