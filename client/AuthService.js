34ac106617ae277fd6a570f69168266405d2d93f
Loren Barrick <lorenbarrick@gmail.com>
2017-01-05 12:25:51 -0800

    another config change

--- client/AuthService.js
+++ client/AuthService.js
@@ -12,6 +12,7 @@ class AuthService extends EventEmitter {
     // console.log('calling loggedIn', this.loggedIn());
     this.lock = new Auth0Lock(clientId, domain, {});
     console.log(this.lock, 'lock');
+    console.log('process.env', process.env);
     console.log('this in authservice', this);
     // Add callback for lock `authenticated` event
     this.lock.on('authenticated', this._doAuthentication.bind(this));
