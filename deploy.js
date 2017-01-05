36178a528c1e9d2f0fddef4c1e063b421cc282e5
Loren Barrick <lorenbarrick@gmail.com>
2017-01-05 09:09:54 -0800

    add console logs to check to see if deploy is running

--- deploy.js
+++ deploy.js
@@ -1,9 +1,11 @@
 // Since postinstall will also run when you run npm install
 // locally we make sure it only runs in production
+console.log('In deploy.js');
 if (process.env.NODE_ENV === 'production') {
   // We basically just create a child process that will run
   // the production bundle command
-  
+  console.log('in the if statement in deploy.js');
+
   var child_process = require('child_process');
   child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
