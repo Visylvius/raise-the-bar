7cfe86761f1bce03ab2e82e7b91f5296610ab17a
Loren Barrick <lorenbarrick@gmail.com>
2017-01-09 13:16:41 -0800

    change cloudinary keys for prod

--- server/cloudinary-helpers.js
+++ server/cloudinary-helpers.js
@@ -1,15 +1,9 @@
 var cloudinary = require('cloudinary');
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
-
 cloudinary.config({
-  cloud_name: 'raise-the-bar',
-  api_key: '542361533446431',
-  api_secret: '_h2HI5kaohXFtDILSX6HDTFFkvo'
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 exports.uploadPhoto = (imgBuffer, imgId) => {
